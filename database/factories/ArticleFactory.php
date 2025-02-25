c6c50fbf94b36a6e0d9bb32c60a2f27d3b918062
Sankar Bala <sankarbala232@gmail.com>
2025-02-25 20:37:04 +0600

    sdfs

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -17,7 +17,12 @@ class ArticleFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word,
+            'slug' => $this->faker->slug,
+            'short_description' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'image' => $this->faker->imageUrl,
+            'published' => $this->faker->numberBetween(0, 1),
         ];
     }
 }
