4473c73b4ba5ffbf1bd4c487db505a0d4591aba3
Sankar Bala <sankarbala232@gmail.com>
2025-02-28 02:14:09 +0600

    sdfs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,12 @@
     <div class="hero-section d-flex align-items-start pt-5">
         <div class="container mt-5">
             <div class="row hero-bg">
-                <div class="col-lg-6">
+                <div class="col-xl-6">
                     <div class="hero-content">
-                        <h3 class="text-warning">Welcome to {{ env('APP_NAME') }}</h3>
+                        <h3 class="welcome">Welcome to {{ env('APP_NAME') }}</h3>
                         {{-- <h2>We produce medicine from nature.</h2> --}}
-                        <h3 class="mt-4">Holistic Health, Inspired by Nature.</h3>
-                        <p class="text-dark  text-justify">At {{ env('APP_NAME') }}, we blend ancient Ayurvedic wisdom with
+                        <h3 class="tagline">Holistic Health, Inspired by Nature.</h3>
+                        <p class="headline text-justify">At {{ env('APP_NAME') }}, we blend ancient Ayurvedic wisdom with
                             modern
                             innovation to
                             create safe, effective, and natural healthcare solutions. With a commitment to quality and
@@ -36,11 +36,12 @@
     </div>
 
     <div class="about-section">
-        <div class="container">
-            <div class="row about-bg">
+        <div class="container-fluid">
+            <div class="row about-bgs">
                 <div class="col-lg-6">
-                    <div class="about-thumb">
-                        <img src="{{ asset('assets/images/sbl/right-logo-t.png') }}" alt="" style="width: 450px;">
+                    <div class="about-thumbss">
+                        <img class="img-thumbnail" src="{{ asset('assets/images/sbl/factory.jpeg') }}" alt=""
+                            style="width:100%;" />
                         {{-- <div class="about-shape">
                             <div class="ab-shape1"><img src="assets/images/resource/about-shape1.png" alt=""></div>
                             <div class="ab-shape2"><img src="assets/images/resource/about-shape2.png" alt=""></div>
@@ -51,9 +52,9 @@
                     <div class="about-right">
                         <div class="dentist-section-title">
                             <h1>Our Division</h1>
-                            <p class="text-justify">Innovative Healthcare Divisions Dedicated to Ayurvedic, Herbal,
-                                Pharmaceutical, and
-                                Veterinary Solutions for Holistic Well-Being and a Healthier Future.</p>
+                            <p class="text-justify border-bottom mb-4">Innovative Healthcare Divisions Dedicated to
+                                Ayurvedic, Herbal,
+                                Pharmaceutical, and Veterinary Solutions for Holistic Well-Being and a Healthier Future.</p>
                         </div>
                         <div class="single-about-right">
 
@@ -70,8 +71,8 @@
                                 </div>
                             @endforeach
 
-                            <div class="btn btn-lg btn-info px-3 py-2 my-4"><a href="about.blade.php">See all products</a>
-                            </div>
+                            {{-- <div class="btn btn-lg btn-info px-3 py-2 my-4"><a href="about.blade.php">See all products</a>
+                            </div> --}}
                         </div>
                     </div>
                 </div>
@@ -200,115 +201,7 @@
         </div>
     </div>
 
-    <div class="choose-us-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="choose_us_left">
-                        <div class="choose-us-title">
-                            <h5>Why choose us our dental care?</h5>
-                            <p>Lorem ipsum dolor sit amet adipiscing elit, eiusmod tempor incididunt ut labore et dolore
-                                magna aliqua ipsum .</p>
-                        </div>
-                        <div class="choose-us-icon-box d-flex">
-                            <div class="choose-us-icon">
-                                <div class="icon"><i class="fas fa-check"></i></div>
-                            </div>
-                            <div class="icon-box-content">
-                                <div class="title">
-                                    <h2>Advanced technology for dental care</h2>
-                                </div>
-                                <div class="description">
-                                    <p>Curabitur sagittis libero tincidunt finibus.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="choose-us-icon-box d-flex">
-                            <div class="choose-us-icon">
-                                <div class="icon"><i class="fas fa-check"></i></div>
-                            </div>
-                            <div class="icon-box-content">
-                                <div class="title">
-                                    <h2>Available emergency dental services</h2>
-                                </div>
-                                <div class="description">
-                                    <p>Curabitur sagittis libero tincidunt finibus.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="choose-us-icon-box d-flex">
-                            <div class="choose-us-icon">
-                                <div class="icon"><i class="fas fa-check"></i></div>
-                            </div>
-                            <div class="icon-box-content">
-                                <div class="title">
-                                    <h2>Our best qualified dentist doctor team</h2>
-                                </div>
-                                <div class="description">
-                                    <p>Curabitur sagittis libero tincidunt finibus.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="choose-us-icon-box d-flex">
-                            <div class="choose-us-icon">
-                                <div class="icon"><i class="fas fa-check"></i></div>
-                            </div>
-                            <div class="icon-box-content">
-                                <div class="title">
-                                    <h2>Our best qualified dentist doctor team</h2>
-                                </div>
-                                <div class="description">
-                                    <p>Curabitur sagittis libero tincidunt finibus.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="section-button"><a href="home.html">Dental Services</a></div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="choose_us_right">
-                        <div class="choose-us-title right">
-                            <h5>Do you have any question?</h5>
-                            <p>Ask your question here. Our agent will reply you soon.</p>
-                        </div>
-                        <div class="form-area contact-form">
-                            <div class="form-inner">
-                                <form id="message-form" class="form-controls rows" action="{{ route('store_message') }}">
-                                    <div class="col-lg-12">
-                                        <div class="form-group">
-                                            <input class="form-control" size="40" placeholder="Name" type="text"
-                                                name="name" required>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <input class="form-control" size="40" value="" type="text"
-                                                name="phone" placeholder="Phone Number" required />
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <input class="form-control" size="40" value="" type="email"
-                                                name="email" placeholder="Email Address" required />
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <textarea class="form-control" cols="20" rows="7" placeholder="Write Message" name="message" required></textarea>
-                                        </div>
-                                        <span id="message-response" class="text-dark"></span>
-                                    </div>
-                                    <div class="col-md-12 text-right">
-                                        <button class="msg-button" type="submit">Send Message</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
 
     <div class="blog-section">
         <div class="container">
@@ -347,6 +240,65 @@
                             </div>
                         </div>
                     @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="choose-us-sectionl">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="dentist-section-title">
+                        <h1>Do you have any question?</h1>
+                        <p>Ask your question here. Our agent will reply you soon.</p>
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4343.456803074698!2d90.41233592574451!3d23.7332092526258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1740685907697!5m2!1sen!2sbd"
+                        width="100%" height="500px" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                </div>
+                <div class="col-lg-5">
+                    <div class="contact-form-wrapper">
+                        <div class="form-area contact-form">
+                            <div class="form-inner">
+                                <form id="message-form" class="form-controls rows"
+                                    action="{{ route('store_message') }}">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <input class="form-control" size="40" placeholder="Name" type="text"
+                                                name="name" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <input class="form-control" size="40" value="" type="text"
+                                                name="phone" placeholder="Phone Number" required />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <input class="form-control" size="40" value="" type="email"
+                                                name="email" placeholder="Email Address" required />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <textarea class="form-control" cols="20" rows="7" placeholder="Write Message" name="message" required></textarea>
+                                            <span id="message-response" class="text-dark"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 text-right">
+                                        <button class="msg-button" type="submit">Send Message</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -415,6 +367,43 @@
 
 @push('styles')
     <style>
+        .hero-section .welcome {
+            font-size: 40px;
+            font-weight: 700;
+            color: #390e70;
+            margin-bottom: 20px;
+        }
+
+        .hero-section .tagline {
+            font-size: 30px;
+            font-weight: 700;
+            color: black;
+            margin-bottom: 20px;
+        }
+
+        .hero-section .headline {
+            font-size: 24px;
+            font-weight: 400;
+            color: black;
+            margin-bottom: 20px;
+            font-family: 'Times New Roman, Times, serif';
+        }
+
+        .hero-section .slider-button a {
+            font-size: 20px;
+            font-weight: 700;
+            color: white;
+            background-color: #390e70;
+            padding: 10px 20px;
+            border-radius: 5px;
+            border: none;
+            cursor: pointer;
+        }
+
+        .about-right {
+            padding-right: 20px;
+        }
+
         .three_line_title a {
             display: -webkit-box;
             -webkit-line-clamp: 2;
@@ -446,6 +435,12 @@
 
         .four_line_short_description:hover {
             color: #111;
+        }
+
+        .contact-form input {
+            border: #000000;
+            border-radius: 5px;
+            color: #000000;
         }
 
         .msg-button {
