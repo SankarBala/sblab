5c8166c7e1211240ff6dc09dc8aeef302d9a531a
Sankar Bala <sankarbala232@gmail.com>
2025-03-08 17:27:27 +0600

    sdf

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -27,25 +27,26 @@
                             {{-- <div class="col-12">
                                 <button class="btn btn-sm btn-info rounded">Read More</button>
                             </div> --}}
-                            <div class="col-12 mt-4">
-
-                                <div class="blog-details-socials float-end">
-                                    <h5 class="d-inline-block  mx-5">Share on Social Media</h5>
-                                    <a class="btn btn-primary text-light px-5"
-                                        href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('article', $article)) }}"
-                                        target="_blank">
-                                        <i class="fab fa-facebook-f"></i>
-                                    </a>
-                                    <a class="btn btn-primary text-light px-5 mx-1"
-                                        href="https://twitter.com/intent/tweet?url={{ urlencode(route('article', $article)) }}"
-                                        target="_blank">
-                                        <i class="fab fa-twitter"></i>
-                                    </a>
-                                    <a class="btn btn-primary text-light px-5"
-                                        href="https://www.linkedin.com/sharing/share-offsite/?url={{ urlencode(route('article', $article)) }}"
-                                        target="_blank">
-                                        <i class="fab fa-linkedin-in"></i>
-                                    </a>
+                            <div class="col-12 my-4">
+                                <div class="blog-details-socials d-flex justify-content-between">
+                                    <h5 class="d-inline-block">Share on Social Media</h5>
+                                    <div>
+                                        <a class="btn btn-primary text-light px-5"
+                                            href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('article', $article)) }}"
+                                            target="_blank">
+                                            <i class="fab fa-facebook-f"></i>
+                                        </a>
+                                        <a class="btn btn-primary text-light px-5 mx-1"
+                                            href="https://twitter.com/intent/tweet?url={{ urlencode(route('article', $article)) }}"
+                                            target="_blank">
+                                            <i class="fab fa-twitter"></i>
+                                        </a>
+                                        <a class="btn btn-primary text-light px-5"
+                                            href="https://www.linkedin.com/sharing/share-offsite/?url={{ urlencode(route('article', $article)) }}"
+                                            target="_blank">
+                                            <i class="fab fa-linkedin-in"></i>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
 
@@ -62,16 +63,16 @@
                             </div> --}}
 
 
-                            <div class="col-lg-12">
+                            <div class="col-12">
                                 <div class="blog-details-content">
                                     <h3>2 Comments</h3>
                                 </div>
                                 <div class="blog-details-comment">
                                     <div class="blog-details-comment-reply">
-                                        <a href="blog-details.html#">Reply</a>
+                                        <button class="btn btn-sm btn-primary">Reply</button>
                                     </div>
                                     <div class="blog-details-comment-thumb">
-                                        <img src="assets/images/resource/comment1.png" alt="">
+                                        <img src="{{ asset('assets/images/resource/comment1.png') }}" alt="">
                                     </div>
                                     <div class="blog-details-comment-content">
                                         <h2>Michael jordan</h2>
@@ -79,50 +80,39 @@
                                         <p>Nulla vitae orci luctus risus tristique sollicitudin sed at quam. Nulla sem
                                             dui, faucibus sit amet justo sed, laoreet ornare leo. </p>
                                     </div>
-                                </div>
-                                <div class="blog-details-comment reply">
-                                    <div class="blog-details-comment-reply">
-                                        <a href="blog-details.html#">Reply</a>
-                                    </div>
-                                    <div class="blog-details-comment-thumb">
-                                        <img src="assets/images/resource/comment2.png" alt="">
-                                    </div>
-                                    <div class="blog-details-comment-content">
-                                        <h2>Angel Jara</h2>
-                                        <span>15 September, 2023</span>
-                                        <p>Hello vitae orci luctus risus tristique sollicitudin sed at quam. Karet sem
-                                            dui, faucibus sit amet justo sed, ornare deo </p>
+
+                                    <div class="blog-details-comment reply">
+                                        <div class="blog-details-comment-reply">
+                                            <button class="btn btn-sm btn-primary">Reply</button>
+                                        </div>
+                                        <div class="blog-details-comment-thumb">
+                                            <img src="{{ asset('assets/images/resource/comment2.png') }}" alt="">
+                                        </div>
+                                        <div class="blog-details-comment-content">
+                                            <h2>Angel Jara</h2>
+                                            <span>15 September, 2023</span>
+                                            <p>Hello vitae orci luctus risus tristique sollicitudin sed at quam. Karet sem
+                                                dui, faucibus sit amet justo sed, ornare deo </p>
+                                        </div>
+                                        @stack('reply')
                                     </div>
                                 </div>
                             </div>
-                            <div class="blog-details-contact">
+                            <div class="col-12">
                                 <div class="blog-details-content">
                                     <h3>Post Comment</h3>
                                 </div>
-                                <form action="https://formspree.io/f/myyleorq" method="POST">
-                                    <div class="row">
-                                        <div class="col-lg-6 col-md-6">
-                                            <div class="input-box">
-                                                <input type="text" name="Name" placeholder="Full Name"
-                                                    autocomplete="off" required>
-                                            </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="user">User Name</div>
+                                        <div class="">
+                                            <textarea class="form-control" name="message" id="answer" rows="6" placeholder="Write here..."></textarea>
                                         </div>
-                                        <div class="col-lg-6 col-md-6">
-                                            <div class="input-box">
-                                                <input type="text" name="Your Email" placeholder="Email Address">
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-12 col-md-12">
-                                            <div class="input-box">
-                                                <textarea name="message" id="message" cols="30" rows="10" placeholder="Your Message..."></textarea>
-                                            </div>
-                                            <div class="input-button">
-                                                <button type="submit">Submit Now <i class="fas fa-arrow-right"></i>
-                                                </button>
-                                            </div>
+                                        <div class="float-end mt-3">
+                                            <button type="submit" class="btn btn-primary">Comment</button>
                                         </div>
                                     </div>
-                                </form>
+                                </div>
                                 <div id="status"></div>
                             </div>
 
@@ -132,6 +122,19 @@
                 <div class="col-lg-4 col-md-12">
                     <x-article-sidebar :article="$article" />
                 </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('reply')
+    <div class="row">
+        <div class="col-12">
+            <div class="">
+                <textarea class="form-control" name="message" id="answer" rows="6" placeholder="Write here..."></textarea>
+            </div>
+            <div class="float-end mt-3">
+                <button type="submit" class="btn btn-primary">Comment</button>
             </div>
         </div>
     </div>
@@ -167,6 +170,39 @@
     <script src="https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore-compat.js"></script>
 
     <script>
+        // Comment box
+        const commentBox = `<div class="row">
+                                <div class="col-12">
+                                    <div class="user">User Name</div>
+                                    <div class="">
+                                        <textarea class="form-control" name="message" id="answer" rows="6" placeholder="Write here..."></textarea>
+                                    </div>
+                                    <div class="float-end mt-3">
+                                        <button type="submit" class="btn btn-primary">Comment</button>
+                                    </div>
+                                </div>
+                            </div>`;
+
+
+        function getCommentBox(username, ) {
+            return `
+        <div class="row">
+            <div class="col-12">
+                <div class="user">${username}</div>
+                <div class="">
+                    <textarea class="form-control" name="message" rows="6" placeholder="Write here..."></textarea>
+                </div>
+                <div class="float-end mt-3">
+                    <button type="submit" class="btn btn-primary">Comment</button>
+                </div>
+            </div>
+        </div>
+    `;
+        }
+
+
+
+
         // Your Firebase Configuration
         const firebaseConfig = {
             apiKey: "AIzaSyAIx0D1BFoUD3CSYieapKhtNKy7S_E8YGo",
---
+++ resources/views/components/comment.blade.php
@@ -0,0 +1,14 @@
+<div class="blog-details-comment reply">
+    <div class="blog-details-comment-reply">
+        <button class="btn btn-sm btn-primary">Reply</button>
+    </div>
+    <div class="blog-details-comment-thumb">
+        <img src="{{ asset('assets/images/resource/comment2.png') }}" alt="User Image" />
+    </div>
+    <div class="blog-details-comment-content">
+        <h2>Angel Jara</h2>
+        <span>15 September, 2023</span>
+        <p>Hello vitae orci luctus risus tristique sollicitudin sed at quam. Karet sem
+            dui, faucibus sit amet justo sed, ornare deo </p>
+    </div>
+</div>
